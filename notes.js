f3ee9e99c62bdb7c562d2e8d7a001ec8f3bb8817
hi <kangja1996@gmail.com>
2020-10-22 11:32:58 -0400

    progress

--- notes.js
+++ notes.js
@@ -45,5 +45,26 @@
 //the same machine will record different times!
 //for fast algorithms, speed measurements may not be precise enough?
 
+// https://rithmschool.github.io/function-timer-demo/
+// WE must decide how algorithm performs when it starts get to infinity. As input n grows to insane level. We are not going to make graph/chart every single time we want to understand how it performs relative to other code. Instead of working with time(rather than counting seconds, which are variable), we will count the NUMBER of simple operations the computer has to perform! We will do it just by looking at the code WITHOUT having to run the code and print graphs/charts. We will roughly approximate what the chart will look like if we were to plot it. 
+
+//Counting operations 
+// EX)
+// function addUpTo(n) {
+//   return n * (n + 1) / 2;
+// }
+//This example has 1 multiplication, 1 addition, and 1 division. It doesn't matter how large n is. Whether n is million or 5, we have the same number of addition, multiplication, and division. It doesn't change. Again, we have 3 simple operations, regardless of the size of n. 
+
+
+//EX)
+// function addUpTo(n) {
+//   let total = 0; //1 assignment
+//   for (let i = 1; i <= n; i++) { //1 assignment, n comparisons, n additions and n assignments
+//     total += i; //we have n additions inside the loop. if n is million, we are adding million times. //n additions, n assignments
+//   }
+//   return total;
+// }
+//this loop is dependent on the value of n. as n grows larger and larger, eventually approaching infinity, the number of operations we will have here that we have to do will grow at least with proportion to n. Therefore, depending on what we count, the number of operations can be as low as 2n or as high as 5n+2. But regardless of the exact number, the number of operations grows roughly proportionally with n. 
+
 
 
